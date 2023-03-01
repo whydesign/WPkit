5f8fa5fa2099df85c391a531c3dd221e759b57da
Ludwig Jäschke <info@whydesign-halle.de>
2023-03-01 14:04:32 +0100

    Enhanced theme function

--- htdocs/wp-content/themes/wpkit/functions.php
+++ htdocs/wp-content/themes/wpkit/functions.php
@@ -70,18 +70,6 @@ function wpkit_setup() {
 		)
 	);
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'wpkit_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
-
 	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
 
@@ -131,6 +119,42 @@ function wpkit_widgets_init() {
 			'after_title'   => '</h2>',
 		)
 	);
+
+    register_sidebar(
+        array(
+            'name'          => esc_html__( 'Footer-1', 'wpkit' ),
+            'id'            => 'footer-1',
+            'description'   => esc_html__( 'Add widgets here.', 'wpkit' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s uk-width-1-4@m">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
+
+    register_sidebar(
+        array(
+            'name'          => esc_html__( 'Footer-2', 'wpkit' ),
+            'id'            => 'footer-2',
+            'description'   => esc_html__( 'Add widgets here.', 'wpkit' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s uk-width-auto uk-width-1-4@m">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
+
+    register_sidebar(
+        array(
+            'name'          => esc_html__( 'Footer-3', 'wpkit' ),
+            'id'            => 'footer-3',
+            'description'   => esc_html__( 'Add widgets here.', 'wpkit' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s uk-width-auto uk-width-1-4@m">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
 }
 add_action( 'widgets_init', 'wpkit_widgets_init' );
 
@@ -147,6 +171,7 @@ function wpkit_scripts() {
     wp_enqueue_script( 'uikit-icons', get_template_directory_uri() . '/node_modules/uikit/dist/js/uikit-icons.js', array(), _S_VERSION, true );
 
     wp_enqueue_script('jquery');
+    wp_enqueue_style( 'dashicons' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -181,3 +206,229 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+function slider_option_enabled(){
+    if ( get_theme_mod( 'slider_activation') ) {
+        return true;
+    }
+    return false;
+}
+
+add_action( 'customize_register', 'uikit_customizer_options' );
+
+function uikit_customizer_options($wp_customize){
+
+    $wp_customize->remove_control('blogdescription');
+
+    $slider_get_posts_list = get_posts( array('numberposts' => 999, 'post_type' => 'header_slider' ));
+
+    $slider_posts_list = [];
+
+    foreach( $slider_get_posts_list as $slider_post ) {
+        $slider_posts_list[$slider_post->ID] = esc_html( get_the_title($slider_post->ID) );
+    }
+
+    $wp_customize->add_setting( 'slider_activation', array(
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control( 'slider_activation_control', array(
+        'type' => 'checkbox',
+        'label' => 'Activate Header-Slider',
+        'priority' => 10,
+        'section' => 'header_image',
+        'settings' => 'slider_activation',
+    ));
+
+    $wp_customize->add_setting( 'slider_select', array(
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control( 'slider_select_control', array(
+        'type' => 'select',
+        'label' => 'Choose your Header-Slider',
+        'priority' => 10,
+        'section' => 'header_image',
+        'settings' => 'slider_select',
+        'choices' => $slider_posts_list,
+        'active_callback' => 'slider_option_enabled',
+    ));
+
+    $wp_customize->add_setting( 'slider_autoplay', array(
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control( 'slider_autoplay_control', array(
+        'type' => 'checkbox',
+        'label' => 'Autoplay Header-Slider',
+        'priority' => 10,
+        'section' => 'header_image',
+        'settings' => 'slider_autoplay',
+        'choices' => $slider_posts_list,
+        'active_callback' => 'slider_option_enabled',
+    ));
+
+    $wp_customize->add_setting( 'slider_dotnav', array(
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control( 'slider_dotnav_control', array(
+        'type' => 'checkbox',
+        'label' => 'Activate Dot-Navigation',
+        'priority' => 10,
+        'section' => 'header_image',
+        'settings' => 'slider_dotnav',
+        'choices' => $slider_posts_list,
+        'active_callback' => 'slider_option_enabled',
+    ));
+
+    $wp_customize->add_setting( 'slider_arrownav', array(
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control( 'slider_arrownav_control', array(
+        'type' => 'checkbox',
+        'label' => 'Activate Arrow-Navigation',
+        'priority' => 10,
+        'section' => 'header_image',
+        'settings' => 'slider_arrownav',
+        'choices' => $slider_posts_list,
+        'active_callback' => 'slider_option_enabled',
+    ));
+
+
+    $wp_customize->add_section( 'wpkit_option_section', array(
+        'title'=> __( 'WPkit Options', 'wpkit_option_section' ),
+        'priority' => 160,
+        'capability' => 'edit_theme_options'
+    ));
+
+    $wp_customize->add_setting( 'searchform_nav', array(
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control( 'searchform_nav_control', array(
+        'type' => 'checkbox',
+        'label' => 'Show Searchform in Navi',
+        'priority' => 10,
+        'section' => 'wpkit_option_section',
+        'settings' => 'searchform_nav',
+    ));
+
+    $wp_customize->add_setting( 'sticky_nav', array(
+        'sanitize_callback' => 'sanitize_text_field',
+    ));
+
+    $wp_customize->add_control( 'sticky_nav_control', array(
+        'type' => 'checkbox',
+        'label' => 'Enable Sticky Nav',
+        'priority' => 10,
+        'section' => 'wpkit_option_section',
+        'settings' => 'sticky_nav',
+    ));
+}
+
+add_action( 'init', 'wpkit_custom_post' );
+
+function wpkit_custom_post() {
+
+    $args_places = array(
+        'labels' => array(
+            'name' => _x( 'Header Slider', 'Alle Slider' ),
+            'singular_name' => _x( 'Header Slider', 'Ein Slider' ),
+            'menu_name' => _x( 'Header Slider', 'Alle Slider'),
+        ),
+        'description' => 'Header Slider',
+        'public' => true,
+        'show_in_rest' => true,
+        'menu_icon' => 'dashicons-slides',
+        'supports' => array( 'title', 'editor', 'thumbnail', 'revisions' ),
+    );
+
+    register_post_type( 'header_slider', $args_places );
+
+}
+
+function block_gallery_ukslider( $block_content, $block ) {
+    if ( $block['blockName'] === 'core/gallery' ) {
+
+        preg_match_all('/<img [^>]+>/', $block_content, $image_matches, PREG_SET_ORDER);
+        preg_match_all('/<figcaption class="wp-element-caption">(.*?)<\/figcaption>/s', $block_content, $caption_matches, PREG_SET_ORDER);
+
+        $content = '<div uk-slideshow="autoplay: '.(get_theme_mod("slider_autoplay") ? "true" : "false").'; ratio: false">';
+        $content .= '<div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">';
+        $content .= '<ul class="uk-slideshow-items" uk-height-viewport="offset-top: true; offset-bottom: 30">';
+
+        foreach ($image_matches as $key=>$image) {
+            $image_full = str_replace('-1024x683.', '.', $image[0]);
+            $image_full = preg_replace('/(<img\b[^><]*)>/i', '$1 uk-cover>', $image_full);
+
+            $content .= '<li>';
+            $content .= $image_full;
+            $content .= '<div class="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">';
+            $content .= $caption_matches[$key][0];
+            $content .= '</div>';
+            $content .= '</li>';
+        }
+
+        $content .= '</ul>';
+
+        if ( get_theme_mod('slider_arrownav') ) {
+            $content .= '<a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>';
+            $content .= '<a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>';
+        }
+
+        $content .= '</div>';
+
+        if ( get_theme_mod('slider_dotnav') ) {
+            $content .= '<ul class="uk-slideshow-nav uk-dotnav uk-flex-center uk-margin"></ul>';
+        }
+
+        $content .= '</div>';
+
+        return $content;
+    }
+
+    return $block_content;
+}
+
+add_filter( 'render_block', 'block_gallery_ukslider', 10, 2 );
+
+function add_class_to_category( $category_list ) {
+    $uk_class = 'uk-button uk-button-text';
+    return str_replace('<a href="',  '<a class="'. $uk_class . '" href="', $category_list);
+}
+
+add_filter('the_category', 'add_class_to_category', 10, 3);
+
+function add_class_to_tags( $tag_list ) {
+    $uk_class = 'uk-button uk-button-text';
+    return str_replace('<a href="',  '<a class="'. $uk_class . '" href="', $tag_list);
+}
+
+add_filter('the_tags', 'add_class_to_tags', 10, 3);
+
+function textleader_custom_box() {
+    add_meta_box(
+        'uk_textleader',
+        'Textvorspann',
+        'post_leader_callback',
+        'post'
+    );
+}
+add_action( 'add_meta_boxes', 'textleader_custom_box' );
+
+function post_leader_callback($post) {
+    $value = get_post_meta( $post->ID, '_uk_textleader_meta_key', true );
+    echo '<textarea style="width:100%" id="text_leader" name="text_leader">' . esc_attr( $value ) . '</textarea>';
+}
+
+function uk_textleader_save_postdata( $post_id ) {
+    if ( array_key_exists( 'text_leader', $_POST ) ) {
+        update_post_meta(
+            $post_id,
+            '_uk_textleader_meta_key',
+            $_POST['text_leader']
+        );
+    }
+}
+add_action( 'save_post', 'uk_textleader_save_postdata' );
